11fd80a9fc0e52e56085c07f3569abbffda6a1ff
Nadelladwi <nadella_dwi_28rpl@student.smktelkom-mlg.sch.id>
2020-04-09 22:34:25 +0700

    sudah benar pak

--- aset/header.php
+++ aset/header.php
@@ -19,7 +19,7 @@ if(!(isset($_SESSION['id_petugas'])))
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#"><i class="fas fa-book-reader text-white mr-2"></i>‘SiPERPUS | Hai, admin’</a>
+  <a class="navbar-brand" href="#"><i class="fas fa-book-reader text-white mr-2"></i>‘SiPERPUS | Hai, <?= $_SESSION['nama_petugas'] ?>’</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
